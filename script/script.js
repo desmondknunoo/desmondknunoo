9537298cd5657dab9d20ce485a551c87f4cae245
Desmond Kwame Nunoo <desmondknunoo@gmail.com>
2022-08-14 11:29:22 +0000

    about page added

--- script/script.js
+++ script/script.js
@@ -1,15 +1,18 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 console.log(header)
-window.onscroll = function () {
-    const top = window.scrollY;
-    if (top >= 100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.add('navbarDark');
-    }
-}
+header.classList.add('navbarDark');
+// window.onscroll = function () {
+//     const top = window.scrollY;
+//     if (top >= 100) {
+//         header.classList.add('navbarDark');
+//     }
+//     else {
+//         header.classList.add('navbarDark');
+//     }
+// }
+
+
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
